b8a62a8e6d62a3c466d9ef584ac9942cb9d80097
rhqshipon <public.rhqshipon@gmail.com>
2025-08-11 20:32:19 +0600

    push new updates

--- [1] theory/Day 5/1. string/cstring.cpp
+++ [1] theory/Day 5/1. string/cstring.cpp
@@ -15,32 +15,36 @@ int main() {
     char str2[MAX_STRING_LEN];
 
     setStrings(str1, str2);
-
-    cout << "str1: " << str1 << endl;
-    cout << "str2: " << str2 << endl;
-    cout << "Length of str1: " << strlen(str1) << endl;
-    cout << "Length of str2: " << strlen(str2) << endl;
-
-    setStrings(str1, str2);
-    cout << "Result of strcat: "<< strcat(str1, str2) << endl;
+    cout << "str1: " << str1 << endl;                    // Output: Hello
+    cout << "str2: " << str2 << endl;                    // Output: World
+    cout << "Length of str1: " << strlen(str1) << endl;  // Output: 5
+    cout << "Length of str2: " << strlen(str2) << endl;  // Output: 5
 
     setStrings(str1, str2);
-    cout << "Result of strncat: "<< strncat(str1, str2, 2) << endl;
+    cout << "Result of strcat: "<< strcat(str1, str2) << endl;  // Output: HelloWorld
 
     setStrings(str1, str2);
-    cout << "Result of str1lwr: " << strlwr(str1) << endl;
+    cout << "Result of strncat: "<< strncat(str1, str2, 2) << endl;  // Output: HelloWo
 
     setStrings(str1, str2);
-    cout << "Result of str2lwr: " << strlwr(str2) << endl;
+    cout << "Result of str1lwr: " << strlwr(str1) << endl;  // Output: hello
 
     setStrings(str1, str2);
-    cout << "Result of str1upr: " << strupr(str1) << endl;
+    cout << "Result of str2lwr: " << strlwr(str2) << endl;  // Output: world
 
     setStrings(str1, str2);
-    cout << "Result of str2upr: " << strupr(str2) << endl;
+    cout << "Result of str1upr: " << strupr(str1) << endl;  // Output: HELLO
 
     setStrings(str1, str2);
-    cout << "Result of strcpy: "<< strcpy(str1, str2) << endl;
+    cout << "Result of str2upr: " << strupr(str2) << endl;  // Output: WORLD
+
+    setStrings(str1, str2);
+    cout << "Result of strcpy: "<< strcpy(str1, str2) << endl;  // Output: World
+
+    setStrings(str1, str2);
+    cout << "Result of strncpy: "<< strncpy(str1, str2, 2) << endl;
+    // Output: DANGEROUS! - prints "Wo" + garbage characters or undefined behavior
+    // because strncpy doesn't null-terminate when n < source length
 
     return 0;
 }
--- [1] theory/Day 5/1. string/string.cpp
+++ [1] theory/Day 5/1. string/string.cpp
@@ -1,46 +1,43 @@
 #include <iostream>
 #include <string>
-#include <cstring>
+
 using namespace std;
 
-void setStrings(char* dest1, char* dest2) {
+void setStrings(string& dest1, string& dest2) {
     dest1 = "Hello";
     dest2 = "World";
 }
 
 int main() {
-    string str1 = "Hello";
-    string str2 = "World";
-    string input;
-    getline(cin, input);
+    string str1, str2;
 
-    //setStrings(str1, str2);
+    setStrings(str1, str2);
+    cout << "str1: " << str1 << endl;                    // Output: Hello
+    cout << "str2: " << str2 << endl;                    // Output: World
+    cout << "Length of str1: " << str1.length() << endl; // Output: 5
+    cout << "Length of str2: " << str2.length() << endl; // Output: 5
 
-    cout << "str1: " << str1 << endl;
-    cout << "str2: " << str2 << endl;
-    cout << "input: " << input << endl;
-    cout << "Length of str1: " << str1.length() << endl;
-    cout << "Length of str2: " << str2.length() << endl;
+    setStrings(str1, str2);
+    cout << "Result of concatenation: " << str1 + str2 << endl; // Output: HelloWorld
 
+    setStrings(str1, str2);
+    cout << "Result of partial concatenation: " << str1 + str2.substr(0, 2) << endl; // Output: HelloWo
 
+    setStrings(str1, str2);
+    cout << "Result of str1 (no case conversion available): " << str1 << endl; // Output: Hello
 
+    setStrings(str1, str2);
+    cout << "Result of str2 (no case conversion available): " << str2 << endl; // Output: World
 
-    cout << "Result of strncat: "<< "" << endl;
+    setStrings(str1, str2);
+    cout << "Result of str1 (no case conversion available): " << str1 << endl; // Output: Hello
 
+    setStrings(str1, str2);
+    cout << "Result of str2 (no case conversion available): " << str2 << endl; // Output: World
 
-    cout << "Result of str1lwr: " << "" << endl;
-
-
-    cout << "Result of str2lwr: " << "" << endl;
-
-
-    cout << "Result of str1upr: " << "" << endl;
-
-
-    cout << "Result of str2upr: " << "" << endl;
-
-
-    cout << "Result of strcpy: "<< "" << endl;
+    setStrings(str1, str2);
+    str1 = str2;
+    cout << "Result of assignment: " << str1 << endl;    // Output: World
 
     return 0;
 }
